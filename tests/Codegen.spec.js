153ab1077a0170add8e12d31339e024def4e2d0f
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-05-27 16:45:21 +0600

    Add codegen

---
+++ tests/Codegen.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/index.html');
+  await page.locator('.card > a').first().click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+});
---
+++ tests/Codegensript.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/index.html');
+  await page.locator('.card > a').first().click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await page.getByRole('link', { name: 'Cart', exact: true }).click();
+  await page.getByRole('button', { name: 'Place Order' }).click();
+  await page.getByText('Place order ×').click();
+});
+//npx playwright test Codegensript.spec.js --project chromium --headed
+// npx playwright codegen -o tests/Codegensript.spec.js
+// npx playwright codegen -o tests/Codegensript.spec.js
